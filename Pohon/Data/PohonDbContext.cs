e49f02531647426c50ce180da88e55d591c2680b
reaganiwadha <reaganiwadha@protonmail.com>
2021-07-16 22:20:34 +0700

    added PohonDbContext

---
+++ Pohon/Data/PohonDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pohon.Data
+{
+    public class PohonDbContext : DbContext
+    {
+        public PohonDbContext(DbContextOptions<PohonDbContext> options) : base(options)
+        {
+            
+        }   
+    }
+}
--- Pohon/Startup.cs
+++ Pohon/Startup.cs
@@ -1,25 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pohon.Data;
 
 namespace Pohon
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration Configuration;
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<PohonDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
