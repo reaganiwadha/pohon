b6db6abf2a272a8354ab6c95e68fb256697af0a1
reaganiwadha <reaganiwadha@protonmail.com>
2021-07-17 06:55:13 +0700

    added base oauth

---
+++ Pohon/Config/GithubOAuthConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Pohon.Config
+{
+    public record GithubOAuthConfig
+    {
+        private const string GithubBaseAuthorizeUri = "https://github.com/login/oauth/authorize";
+            
+        public string ClientId { get; init; }
+        public string ClientSecret { get; init; }
+        public string RedirectUri { get; init; }
+
+        public string AuthorizeUri => $"{GithubBaseAuthorizeUri}/?client_id={ClientId}&redirect_uri={RedirectUri}";
+    }
+}
--- Pohon/Startup.cs
+++ Pohon/Startup.cs
@@ -5,23 +5,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Pohon.Config;
 using Pohon.Data;
 
 namespace Pohon
 {
     public class Startup
     {
-        private readonly IConfiguration Configuration;
+        private readonly IConfiguration _configuration;
         
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<GithubOAuthConfig>(_configuration.GetSection("OAuth:Github"));
+            services.AddDbContext<PohonDbContext>(options => options.UseMySQL(_configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
-            services.AddDbContext<PohonDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,6 +40,8 @@ namespace Pohon
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
