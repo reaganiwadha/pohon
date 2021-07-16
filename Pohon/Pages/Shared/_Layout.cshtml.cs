aed7b2935f7d158e24e969007dda0ddea33fb5b5
reaganiwadha <reaganiwadha@protonmail.com>
2021-07-16 19:40:24 +0700

    init

---
+++ Pohon/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pohon.Pages.Shared
+{
+    public class _Layout : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Pohon/Pages/index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pohon.Pages
+{
+    public class Main : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
